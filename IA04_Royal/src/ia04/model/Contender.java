75d73cfa7f8089e06ee917e6bac503f4ec0a8723
raph <raphael.lauras@gmail.com>
2017-05-30 18:59:34 +0200

    add commentaire

--- IA04_Royal/src/ia04/model/Contender.java
+++ IA04_Royal/src/ia04/model/Contender.java
@@ -7,6 +7,7 @@ public class Contender extends MySteppable{
 	public Contender(int x, int y) {
 		super(x, y);
 		x = 3;
+		//ligne écrite par raph à 18:58
 		// TODO Auto-generated constructor stub
 	}
 
