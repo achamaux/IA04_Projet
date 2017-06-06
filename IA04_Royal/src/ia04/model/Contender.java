ed9edc3964793bcba47f4b2ce544cb9d3d69320b
coffinpa <pauline.coffin@etu.utc.fr>
2017-06-06 18:06:31 +0200

    Food gestion implemented, more strategy certainly needs to be revised.

--- IA04_Royal/src/ia04/model/Contender.java
+++ IA04_Royal/src/ia04/model/Contender.java
@@ -10,7 +10,7 @@ import sim.field.grid.ObjectGrid2D;
 import sim.util.Int2D;
 
 public class Contender extends MySteppable {
-	
+
 	public static int DIST_PERCEPTION = 5;
 	public static int MAX_DEP = 4;
 	public static int MAX_ENERGIE = 20;
@@ -23,17 +23,14 @@ public class Contender extends MySteppable {
 	public static int NOURRITURE_MAX = 4;
 	public static int ENERGIE_PAR_BOUFFE = 4;
 	public static int BOUFFE_CRITIQUE = DIST_PERCEPTION;
-	
+
 	public int vie;
 	public int attaque;
 	public int energie = MAX_ENERGIE;
 	public int distancePerception = DIST_PERCEPTION;
 	public int nourriture = 0;
 
-
 	private Beings beings;
-	
-	
 
 	public Contender(int x, int y, int attaque, int vie, int energie) {
 		super(x, y);
@@ -41,7 +38,7 @@ public class Contender extends MySteppable {
 		this.attaque = attaque;
 		this.energie = energie;
 	}
-	
+
 	public Contender(int x, int y) {
 		super(x, y);
 		Random rand = new Random();
@@ -55,35 +52,47 @@ public class Contender extends MySteppable {
 	@Override
 	public void step(SimState state) {
 		beings = (Beings) state;
+		boolean roundDone = false;
 		if (vie <= 0)
 			meurt(beings);
 		else {
 			System.out.println("\n\n Contender begins step : vie=" + vie +" ; energie =" + energie);
 			/****************Traitement bouffe*****************/
 			if(energie < BOUFFE_CRITIQUE){
-				
-			}
-			
-			
-			/****************Traitement enemi*****************/
-			Contender closestEnemy = getClosestEnemy();
-			// TODO: Va vers lui si dist>1, castagne sinon (tape, l'autre
-			// devrait nous taper aussi normalement)
-			if (closestEnemy != null) {
-				if (!isAtRange(closestEnemy)) {
-					MoveTowards(closestEnemy.x, closestEnemy.y, 1);
-					//si on emp�che le d�placement vers l'endroit o� on est, certains contestants ne bougent pas,
-					//je sais pas pourquoi
-				} else {
-					System.out.println("ennemi � port�e, je le tape ou je fuis");
-					if (closestEnemy.attaque * 2 > vie)
-						escapeFrom(closestEnemy);
-					else
-						attack(closestEnemy);
+				System.out.println("Energy running low");
+				if(nourriture > 0){
+					eat();
+					roundDone = true;
 				}
-			} else {
-				// pas d'ennemis trouv�, marche vers le centre
-				MoveTowards(Beings.GRID_SIZE / 2, Beings.GRID_SIZE / 2, 1);
+				else 
+				{
+					Nourriture food = getClosestFood();
+					if(food != null && food.quantite > 0){
+						takeFood(food);
+						roundDone = true;
+					}
+				}
+			}
+			if(!roundDone)
+			{
+				/****************Traitement enemi*****************/
+				Contender closestEnemy = getClosestEnemy();
+				if (closestEnemy != null) {
+					if (!isAtRange(closestEnemy)) {
+						MoveTowards(closestEnemy.x, closestEnemy.y, 1);
+						//si on emp�che le d�placement vers l'endroit o� on est, certains contestants ne bougent pas,
+						//je sais pas pourquoi
+					} else {
+						System.out.println("ennemi � port�e, je le tape ou je fuis");
+						if (closestEnemy.attaque * 2 > vie)
+							escapeFrom(closestEnemy);
+						else
+							attack(closestEnemy);
+					}
+				} else {
+					// pas d'ennemis trouv�, marche vers le centre
+					MoveTowards(Beings.GRID_SIZE / 2, Beings.GRID_SIZE / 2, 1);
+				}
 			}
 		}
 	}
@@ -95,7 +104,7 @@ public class Contender extends MySteppable {
 		for (int i = 1; i <= distancePerception; i++) {
 			Contender closestEnemy = findEnemyAtRange(i);
 			if (closestEnemy != null) {
-				System.out.println("ClosestEnemy found, at "+ closestEnemy.x + " ; " + closestEnemy.y);
+				System.out.println("ClosestEnemy found, at " + closestEnemy.x + " ; " + closestEnemy.y);
 				return closestEnemy;
 			}
 		}
@@ -127,8 +136,7 @@ public class Contender extends MySteppable {
 		System.out.println("Currently at (" + x + "," + y + ")");
 		System.out.println("moving towards (" + x2 + "," + y2 + ")");
 		int i, dx, dy;
-		for (i = 0 ; i < dist ; i++)
-		{
+		for (i = 0; i < dist; i++) {
 			dx = x2 - x;
 			dy = y2 - y;
 			if (Math.abs(dx) > Math.abs(dy)) {
@@ -136,7 +144,7 @@ public class Contender extends MySteppable {
 					beings.yard.set(x, y, null);
 					x++;
 					beings.yard.set(x, y, this);
-				} else if (dx < 0){
+				} else if (dx < 0) {
 					beings.yard.set(x, y, null);
 					x--;
 					beings.yard.set(x, y, this);
@@ -146,18 +154,18 @@ public class Contender extends MySteppable {
 					beings.yard.set(x, y, null);
 					y++;
 					beings.yard.set(x, y, this);
-				} else if (dy < 0){
+				} else if (dy < 0) {
 					beings.yard.set(x, y, null);
 					y--;
 					beings.yard.set(x, y, this);
 				}
 			}
 		}
-		
+
 		if (energie > 0)
-			energie = energie - dist*ENERGIE_PAR_DEP;
+			energie = energie - dist * ENERGIE_PAR_DEP;
 		else
-			vie --;
+			vie--;
 		System.out.println("Now at " + x + "," + y);
 	}
 
@@ -171,53 +179,51 @@ public class Contender extends MySteppable {
 		cont.vie -= attaque;
 		energie -= ENERGIE_PAR_ATT;
 	}
-	
-	//se déplace dans la direction opposée à celle de l'enemi
-	public void escapeFrom(Contender cont){
+
+	// se déplace dans la direction opposée à celle de l'enemi
+	public void escapeFrom(Contender cont) {
 		int dirx, diry;
 		int dx = cont.x - x;
 		int dy = cont.y - y;
-		dirx = (dx > 0)? x - MAX_DEP : x + MAX_DEP;
+		dirx = (dx > 0) ? x - MAX_DEP : x + MAX_DEP;
 		if (dirx > Beings.GRID_SIZE)
 			dirx = Beings.GRID_SIZE - 1;
 		if (dirx < 0)
 			dirx = 0;
-		diry = (dy > 0)? y - MAX_DEP : y + MAX_DEP;
+		diry = (dy > 0) ? y - MAX_DEP : y + MAX_DEP;
 		if (diry > Beings.GRID_SIZE)
 			diry = Beings.GRID_SIZE - 1;
-		if(diry > 0)
+		if (diry > 0)
 			diry = 0;
 
 		System.out.println("Now escaping from enemy at " + cont.x + " ; " + cont.y);
 		MoveTowards(dirx, diry, MAX_DEP);
 	}
-	
-	public void eat(){
-		while(energie < MAX_ENERGIE - ENERGIE_PAR_BOUFFE && nourriture > 0)
-		{
-			nourriture --;
+
+	public void eat() {
+		while (energie < MAX_ENERGIE - ENERGIE_PAR_BOUFFE && nourriture > 0) {
+			nourriture--;
 			energie += ENERGIE_PAR_BOUFFE;
+			System.out.println("Food eaten ! Food left :" + nourriture + " ; enery = " + energie);
 		}
 	}
-	
-	
+
 	// trouve l'enn//emi le plus proche, retourne null si aucun n'est visible
-		public Nourriture getClosestFood() {
-			// teste toutes les distances pour trouver le plus proche
-			// (v�rifier si getNeighbors classe pas d�j� par proximit�)
-			for (int i = 1; i <= distancePerception; i++) {
-				Nourriture closestFood = findFoodAtRange(i);
-				if (closestFood != null) {
-					System.out.println("ClosestEnemy found, at "+ closestFood.x + " ; " + closestFood.y);
-					return closestFood;
-				}
+	public Nourriture getClosestFood() {
+		// teste toutes les distances pour trouver le plus proche
+		// (v�rifier si getNeighbors classe pas d�j� par proximit�)
+		for (int i = 1; i <= distancePerception; i++) {
+			Nourriture closestFood = findFoodAtRange(i);
+			if (closestFood != null) {
+				System.out.println("ClosestEnemy found, at " + closestFood.x + " ; " + closestFood.y);
+				return closestFood;
 			}
-			System.out.println("No contender found within range " + distancePerception);
-			return null;
 		}
-	
-		
-	public Nourriture findFoodAtRange(int range){
+		System.out.println("No contender found within range " + distancePerception);
+		return null;
+	}
+
+	public Nourriture findFoodAtRange(int range) {
 		Bag b = beings.yard.getNeighborsMaxDistance(x, y, range, true, null, null, null);
 		// retourne le premier contender � une distance range
 		for (Object o : b) {
@@ -232,13 +238,14 @@ public class Contender extends MySteppable {
 		}
 		return null;
 	}
-	
-	public void takeFood(Nourriture food){
-		while(food.quantite >0 && nourriture < NOURRITURE_MAX){
-			nourriture ++;
+
+	public void takeFood(Nourriture food) {
+		while (food.quantite > 0 && nourriture < NOURRITURE_MAX) {
+			nourriture++;
 			food.quantite--;
+			System.out.println("Food taken ! Food left :" + nourriture + " ; enery = " + energie);
 		}
-		energie --;
+		energie--;
 	}
-	
+
 }
