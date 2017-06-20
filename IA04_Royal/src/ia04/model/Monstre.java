9b56c0927a1fc9b9f186110140c97b2124ab4b16
raph <raphael.lauras@gmail.com>
2017-06-20 10:09:10 +0200

    monstres s'arretent quand un seul contender

--- IA04_Royal/src/ia04/model/Monstre.java
+++ IA04_Royal/src/ia04/model/Monstre.java
@@ -6,15 +6,18 @@ import ia04.model.Map.Zone;
 import sim.engine.SimState;
 import sim.util.Bag;
 
-public class Monstre extends Personnage{
+public class Monstre extends Personnage {
 
 	public static int DIST_PERCEPTION = 10;
 	public static int MAX_DEP = 2;
 	public static int ATTAQUE = 10;
 	public static int VIE = 25;
 	public static int MAX_ENERGIE = 50;
-	
-	public enum Type {KRAKEN, TAUREAU};
+
+	public enum Type {
+		KRAKEN, TAUREAU
+	};
+
 	public Type t;
 	public int maxDeplacement;
 	private static final long serialVersionUID = -8909074924721698002L;
@@ -26,26 +29,24 @@ public class Monstre extends Personnage{
 		this.energie = energie;
 		this.distancePerception = distancePerception;
 	}
-	
+
 	private Beings beings;
 
-	public Monstre(int x, int y, int vie, int attaque, int energie, int distancePerception,Type _t, Beings b ) {
+	public Monstre(int x, int y, int vie, int attaque, int energie, int distancePerception, Type _t, Beings b) {
 		super(x, y, vie, attaque, energie, distancePerception);
 		beings = b;
-		if (_t ==Type.KRAKEN) {
+		if (_t == Type.KRAKEN) {
 			t = Type.KRAKEN;
 			attaque = ATTAQUE;
 			vie = VIE;
-			distancePerception = DIST_PERCEPTION*2;
+			distancePerception = DIST_PERCEPTION * 2;
 			maxDeplacement = MAX_DEP;
-			
-		}
-		else {
+		} else {
 			t = Type.TAUREAU;
-			attaque = ATTAQUE/2;
-			vie = (VIE*3)/5;
+			attaque = ATTAQUE / 2;
+			vie = (VIE * 3) / 5;
 			distancePerception = DIST_PERCEPTION;
-			maxDeplacement = MAX_DEP*2;
+			maxDeplacement = MAX_DEP * 2;
 		}
 		this.vie = vie;
 		this.attaque = attaque;
@@ -57,34 +58,36 @@ public class Monstre extends Personnage{
 	public void step(SimState state) {
 		beings = (Beings) state;
 		boolean roundDone = false;
-		if (vie <= 0){
+		if (vie <= 0) {
 			meurt(beings);
-		}
-		else {
-			System.out.println("\n\n Monster begins step : vie=" + vie +" ; energie =" + energie);
+		} else {
+			System.out.println("\n\n Monster begins step : vie=" + vie + " ; energie =" + energie);
 			System.out.println("Currently at (" + x + "," + y + ")");
-			
-			/****************Traitement ennemi*****************/
-			Personnage closestEnemy = getClosestEnemy(beings);
-			if ((closestEnemy != null) && (closestEnemy.getClass()!=Monstre.class)){
-				if (!isAtRange(closestEnemy, 1)) {
-					MoveTowards(closestEnemy.x, closestEnemy.y, 1, beings, 0);
-					roundDone = true;
-				} else {
-					System.out.println("ennemi � port�e, je le tape ou je fuis");
-					//Un monstre n'attaque que les contenders
-					if (closestEnemy.getClass() == Contender.class) attack(closestEnemy, 0);
-					else MoveTowards(Beings.GRID_SIZE, Beings.GRID_SIZE / 2, maxDeplacement, beings, 0);
 
-				}
-			} else {
-				MoveTowards(Beings.GRID_SIZE, Beings.GRID_SIZE / 2, maxDeplacement, beings, 0);
-				roundDone = true;
-				}
+			if (beings.livingContenders > 1) {
+				/**************** Traitement ennemi *****************/
+				Personnage closestEnemy = getClosestEnemy(beings);
+				if ((closestEnemy != null) && (closestEnemy.getClass() != Monstre.class)) {
+					if (!isAtRange(closestEnemy, 1)) {
+						MoveTowards(closestEnemy.x, closestEnemy.y, 1, beings, 0);
+						roundDone = true;
+					} else {
+						System.out.println("ennemi � port�e, je le tape ou je fuis");
+						// Un monstre n'attaque que les contenders
+						if (closestEnemy.getClass() == Contender.class)
+							attack(closestEnemy, 0);
+						else
+							MoveTowards(Beings.GRID_SIZE, Beings.GRID_SIZE / 2, maxDeplacement, beings, 0);
+
+					}
+				} else {
+					MoveTowards(Beings.GRID_SIZE, Beings.GRID_SIZE / 2, maxDeplacement, beings, 0);
+					roundDone = true;
 				}
 			}
-	
-	
+		}
+	}
+
 	// retourne l'objet Map de la case
 	public Map getMap(int x, int y) {
 		Bag b = beings.yard.getObjectsAtLocation(x, y);
@@ -97,5 +100,3 @@ public class Monstre extends Personnage{
 		return null;
 	}
 }
-
-
